2d9fff7954ef532cc76acf3f9b882d6befca7187
EXLUZIV <pegasfasmain.gmail.com>
2022-02-05 17:33:05 +0200

    change if to switch

--- app/Classes/Controller/RouteClass.php
+++ app/Classes/Controller/RouteClass.php
@@ -20,76 +20,73 @@ class RouteClass
 		}
 		
 
-		if ($method === 'GET') {
+		switch ($method) {
+			case "GET":
 
-			$get = new GetClass($connectBD);
+				$get = new GetClass($connectBD);
 			
-			if ($params[3] === 'users') {
-				$get->getByUsers();	
-				
-				return;
-			}
+				if ($params[3] === 'users') {
+					$get->getByUsers();	
+
+					break;
+				}
 			
-			if ($params[3] === 'posts') {
-				if (isset($params[4])) {
-					$get->getById($id);
+				if ($params[3] === 'posts') {
+					if (isset($params[4])) {
+						$get->getById($id);
 	
-					return;
-				} else {
-					$get->getAll();
+						break;
+					} else {
+						$get->getAll();
 	
-					return;
-				}	
-			}
+						break;
+					}	
+				}
 			
-			return;
+			case "POST":
+
+				if ($type === 'posts') {
+
+					$data = $_POST;
+					$userId = $data['user_id'];
+					$postTitle = $data['post_title'];
+					$postText = $data['post_text'];
+	
+					$post = new PostClass($connectBD);
+					$post->post($userId, $postTitle, $postText);
+	
+					break;
+				}
+	
+				break;
+			
+				case "PATCH": 
+
+					if (isset($id)) {
+						$data = file_get_contents('php://input');
+						$data = json_decode($data, true);
+		
+						$userId = $data['user_id'];
+						$postTitle = $data['post_title'];
+						$postText = $data['post_text']; 
+		
+						$patch = new PatchClass($connectBD);
+						$patch->patch($id, $userId, $postTitle, $postText);
+		
+						break;
+					}
+		
+					break;
+
+				case "DELETE": 
+
+					if (isset($id)) {
+						$delete = new DeleteClass($connectBD);
+						$delete->delete($id);
+		
+						break;
+					}
+					break;
 		}
-
-		if ($method === 'POST') {
-
-			if ($type === 'posts') {
-
-				$data = $_POST;
-				$userId = $data['user_id'];
-				$postTitle = $data['post_title'];
-				$postText = $data['post_text'];
-
-				$post = new PostClass($connectBD);
-				$post->post($userId, $postTitle, $postText);
-
-				return;
-			}
-
-			return;
-		}
-
-		if ($method === 'PATCH') {
-			if (isset($id)) {
-				$data = file_get_contents('php://input');
-				$data = json_decode($data, true);
-
-				$userId = $data['user_id'];
-				$postTitle = $data['post_title'];
-				$postText = $data['post_text']; 
-
-				$patch = new PatchClass($connectBD);
-				$patch->patch($id, $userId, $postTitle, $postText);
-
-				return;
-			}
-
-			return;
-		}
-
-		if ($method === 'DELETE') {
-			if (isset($id)) {
-				$delete = new DeleteClass($connectBD);
-				$delete->delete($id);
-
-				return;
-			}
-			return;
-		}
-		return;
 	}
 }
